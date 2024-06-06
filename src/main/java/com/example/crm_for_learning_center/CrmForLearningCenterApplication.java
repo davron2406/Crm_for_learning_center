1a14a4a28ffd5823fc45ddccf432a652479f5ddd
profix <davronsaydullayev2406@gmail.com>
2024-06-06 12:03:38 +0500

    Docker file was added

---
+++ src/main/java/com/example/crm_for_learning_center/CrmForLearningCenterApplication.java
@@ -0,0 +1,13 @@
+package com.example.crm_for_learning_center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmForLearningCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmForLearningCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crm_for_learning_center/CrmForLearningCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crm_for_learning_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmForLearningCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
